d1d07981d888cf65be8bdb6a4dd346b221b81499
nayanjain123 <nayanjain632@gmail.com>
2024-06-07 23:33:00 +0530

    adapters code

---
+++ AdaptersApp/app/src/main/java/com/nayan/adaptersapp/MainActivity.java
@@ -0,0 +1,44 @@
+package com.nayan.adaptersapp;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //AdapterView
+        listView=findViewById(R.id.listview);
+
+        //data source a array or list or a cursor from some database or anything such
+        String[] countries = {"USA","Germany","Saudi","France"};
+
+        //Adapter acts as a bridge b/w the data source and the adapter view. binds the data to the views inside adapterview
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,countries);
+
+        //setting the adapter on the view
+        listView.setAdapter(adapter);
+
+
+
+    }
+
+}
